30cc3c5581ba02c0be2b5d49d5613d99332f11bf
Shu Takayama <syu.takayama@gmail.com>
2020-10-21 18:01:09 +0900

    enable vue-material

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueWeb3 from './plugins/vue-web3'
 import VueTruffleContract from './plugins/vue-truffle-contract'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 import './registerServiceWorker'
 import router from './router'
 
 Vue.use(VueWeb3)
 Vue.use(VueTruffleContract)
+Vue.use(VueMaterial)
 
 Vue.config.productionTip = false
 
